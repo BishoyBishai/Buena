6aa6b7c52dd1a0ae57fd29af2ac42611a66c6bae
bishoy.bishai <bishoy.bishai@gmail.com>
2024-01-17 14:12:43 +0200

    feat(ui): add layout components footer,nav, and layout

---
+++ src/components/layout/Logo.tsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <div className="ml-4 flex lg:ml-0 font-bold text-primary font-handlee">
+      <div className="flex justify-center items-center gap-2">
+        <p className="text-2xl leading-none">Buena</p>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
