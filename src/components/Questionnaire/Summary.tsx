bbaa5bab6ca382d6b3fca67c824c97b9a9a9cb47
bishoy.bishai <bishoy.bishai@gmail.com>
2024-01-18 06:10:22 +0200

    feat(animation): add some animations for the summary component

--- src/components/Questionnaire/Summary.tsx
+++ src/components/Questionnaire/Summary.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 import { Button } from "../ui/Button";
 import { toPlainText } from "@/lib/toPlainText";
 import { getQuestionAnswer } from "./utils";
+import { motion } from "framer-motion";
 
 interface ISummaryProps {
   questions: TQuestion[];
@@ -16,23 +17,35 @@ const Summary: FC<ISummaryProps> = ({
   onResetQuestionnaire,
 }) => {
   return (
-    <div className="py-8 w-full  md:w-1/4  flex flex-col">
+    <div className="py-8 w-full md:w-1/4 flex flex-col">
       <h3 className="text-lg font-bold"> Here is your answers</h3>
-      {questions.map((question) => (
-        <div key={question.name} className="flex gap-4 py-2 justify-between">
+      {questions.map((question, index) => (
+        <motion.div
+          key={question.name}
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: index * 0.5 }}
+          className="flex gap-4 py-2 justify-between"
+        >
           <div className="flex gap-2">
             {question.icon}
             <p className="capitalize">{toPlainText(question.name)}:</p>
           </div>
           <div>{getQuestionAnswer(question, answers)}</div>
-        </div>
+        </motion.div>
       ))}
-      <Button
-        className="self-center mt-0 w-1/2 md:w-auto md:self-end"
-        onClick={onResetQuestionnaire}
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: questions.length * 0.5 }}
       >
-        Reset
-      </Button>
+        <Button
+          className="self-center mt-0 w-1/2 md:w-auto md:self-end"
+          onClick={onResetQuestionnaire}
+        >
+          Reset
+        </Button>
+      </motion.div>
     </div>
   );
 };
